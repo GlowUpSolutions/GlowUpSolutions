cb5326b2761439b2d27384f085c4d5889aeb887d
Shotaro Kanda <glowupsolutions.0515@gmail.com>
2024-11-16 20:05:08 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,6 +3,20 @@ function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
 
+// スクロールイベントのリスナーを設定
+window.addEventListener('scroll', function() {
+  const servicesSection = document.getElementById('services');
+  
+  // サービスセクションが画面に表示されたら
+  const rect = servicesSection.getBoundingClientRect();
+  if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+    servicesSection.classList.add('scrolled');
+  } else {
+    servicesSection.classList.remove('scrolled');
+  }
+});
+
+
 // フォーム送信イベント
 document.getElementById('contact-form').addEventListener('submit', function (event) {
   event.preventDefault();
